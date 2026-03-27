2d90afa67037316b73d2d24b7ea4d61475abed23
ksener <blogkeremsener@gmail.com>
2026-03-27 19:25:28 +0300

    Added ft_count_harvest_iterative.py and ft_count_harvest_recursive.py (ex5)

---
+++ ex5/ft_count_harvest_iterative.py
@@ -0,0 +1,10 @@
+def ft_count_harvest_iterative():
+    i = 1
+    number = int(input("Days until harvest:"))
+    while i <= number:
+        print("Day", i)
+        i = i + 1
+    print("Harvest time!")
+
+
+ft_count_harvest_iterative()
