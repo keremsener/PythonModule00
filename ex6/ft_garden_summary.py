f4c7545e63face06ffa0c05f63e98af297a04ab2
ksener <blogkeremsener@gmail.com>
2026-03-27 19:30:53 +0300

    Added ft_garden_summary.py (ex6)

---
+++ ex6/ft_garden_summary.py
@@ -0,0 +1,10 @@
+def ft_garden_summary():
+    garden = input("Enter garden name: ")
+    plants = input("Enter number of plants: ")
+
+    print("Garden:", garden)
+    print("Plants:", plants)
+    print("Status: Growing well!")
+
+
+ft_garden_summary()
